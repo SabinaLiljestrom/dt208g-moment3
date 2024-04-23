742389c0f4e70342a7bff55f21a001056cd8b4c8
Sabina Liljeström <sabinaliljestrom@Sabinas-MacBook-Pro-2.local>
2024-04-23 16:24:05 +0200

    lagt in en bild i footern och en på about sidan samt börjat skapa konverter funktionen

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.css'
 })
 export class ConvertComponent {
+//properties
+num1: number = 0;
+result: number = 0; 
+x: number = 3.2808;
 
+//methods
+meter(): void {
+  this.result = this.num1 * this.x;
+}
+
+feet (): void {
+  this.result = this.num1 / this.x; 
+}
 }
